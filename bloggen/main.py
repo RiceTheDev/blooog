c33ab35ad4cb560e680ebe601c440c863482582b
raice <ricenoob@toke.com>
2026-08-30 09:25:37 -0300

    fix: call sitemap and rss functions

--- bloggen/main.py
+++ bloggen/main.py
@@ -13,6 +13,8 @@ def main():
 
     homepage.generate_homepage_from_posts(md_posts)
     md.generate_html_from_markdown(md_posts)
+    md.generate_sitemap(md_posts)
+    md.generate_rss(md_posts)
 
 if __name__ == "__main__":
     main()
