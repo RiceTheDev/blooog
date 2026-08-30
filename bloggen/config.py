7810461643125fb18fbed24aed95453e92ca01aa
raice <ricenoob@toke.com>
2026-08-30 09:25:03 -0300

    feat(bloggen): more config options

--- bloggen/config.py
+++ bloggen/config.py
@@ -17,3 +17,13 @@ OUT_HOME_PATH = "./index.html"
 
 POST_TEMPLATE_PATH = "./templates/post_template.html"
 OUT_POST_PATH = "./posts/"
+
+SITE_URL = "https://raice.lol"
+SITE_NAME = "raice's space"
+SITE_DESCRIPTION = "my little personal blog about programming, emulation, old hardware, low level stuff and random thoughts"
+DEFAULT_AUTHOR = "raice"
+DEFAULT_KEYWORDS = ["blog", "programming", "emulation", "old hardware", "low level", "cybersecurity"]
+DEFAULT_OG_IMAGE = "/assets/og-image.png"
+
+SITEMAP_PATH = "./sitemap.xml"
+RSS_PATH = "./rss.xml"
